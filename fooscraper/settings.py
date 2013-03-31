fd08d5ea45b60f87a975727913ea69c1da5b6839
brogupta <bgupta91@gmail.com>
2013-03-30 21:15:03 -0400

    add initial setup/config files

---
+++ fooscraper/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for fooscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'fooscraper'
+
+SPIDER_MODULES = ['fooscraper.spiders']
+NEWSPIDER_MODULE = 'fooscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'fooscraper (+http://www.yourdomain.com)'

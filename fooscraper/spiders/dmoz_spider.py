4c48bf0c7cbc07c9fc3d2b69756a722141da76ad
brogupta <bgupta91@gmail.com>
2013-03-30 22:22:41 -0400

    add xpath functionality to spider

--- fooscraper/spiders/dmoz_spider.py
+++ fooscraper/spiders/dmoz_spider.py
@@ -4,9 +4,16 @@ class DmozSpider(BaseSpider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
         ]
     def parse(self, response):
+      hxs = HtmlXPathSelector(response)
+      sites = hxs.select('//ul/li')
+      for site in sites:
+        title = site.select('a/text()').extract()
+        link = site.select('a/@href').extract()
+        desc = site.select('text()').extract()
+        print title, link, desc
       filename = response.url.split("/")[-2]
       open(filename, 'wb').write(response.body)
